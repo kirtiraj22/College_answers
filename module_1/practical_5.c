d0b4c81f846576d76185b01c6821d470ba911006
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2022-01-28 12:50:41 +0530

    Create practical_5.c
    Q)WAP to display size of every data type using "sizeof" operator.
    
    
    Size of INTEGER is : 4
    Size of FLOAT is : 4
    Size of DOUBLE is : 8
    Size of CHAR is : 1
    Size of LONG is : 4

---
+++ module_1/practical_5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Size of INTEGER is : %d\n",sizeof(int));
+    printf("Size of FLOAT is : %d\n",sizeof(int));
+    printf("Size of DOUBLE is : %d\n",sizeof(double));
+    printf("Size of CHAR is : %d\n",sizeof(char));
+    printf("Size of LONG is : %d\n",sizeof(long));
+    return 0;
+}
+
+
