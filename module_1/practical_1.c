7536bf54248fa0261ce084bd5ce67e746579e427
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2022-01-28 11:12:19 +0530

    Create practical_1.c
    Q)WAP that works as basic calculator.
    
    OUTPUT:
    Enter numbers : 100 2
    Addition is : 102
    Subtraction is : 98
    Multplication is : 200
    Division is : 50

---
+++ module_1/practical_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1,num2;
+    printf("Enter numbers : ");
+    scanf("%d %d",&num1,&num2);
+    printf("Addition is : %d\n",num1+num2);
+    printf("Subtraction is : %d\n",num1-num2);
+    printf("Multplication is : %d\n",num1*num2);
+    printf("Division is : %d\n",num1/num2);
+    return 0;
+}
