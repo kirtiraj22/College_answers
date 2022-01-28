78d0216f93bd9c94b8ad11333303d1f7d6d4b97f
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2022-01-28 11:53:52 +0530

    Create practical_4(B).c
    Q)WAP to  interchange two variables(using third variable)
    
    SAMPLE OUTPUT:
    Enter number 1:30
    Enter number 2:40
    Numbers before replacing are 30 40
    
    Numbers after replacing are 40 30

---
+++ module_1/practical_4(B).c
@@ -0,0 +1,21 @@
+//using third variable
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int num1,num2,temp;
+    printf("Enter number 1:");
+    scanf("%d",&num1);
+    printf("Enter number 2:");
+    scanf("%d",&num2);
+    printf("Numbers before replacing are %d %d\n\n",num1,num2);
+    temp=num2;
+    num2=num1;
+    num1=temp;
+    printf("Numbers after replacing are %d %d\n",num1,num2);
+    return 0;
+}
+
+
