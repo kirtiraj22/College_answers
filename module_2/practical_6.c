f78cca5659e6543ee012b883c26f70227ac3bcb5
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2022-01-28 15:27:00 +0530

    Create practical_6.c
    Q)WAP to read numbers from 1 to 7 and point relatively day sunday to saturday
    
    SAMPLE OUTPUT 1:
    Enter any number between 1 to 7:1
    MONDAY
    
    SAMPLE OUTPUT 2:
    Enter any number between 1 to 7:2
    TUESDAY
    
    SAMPLE OUTPUT 3:
    Enter any number between 1 to 7:3
    WEDNESDAY
    
    SAMPLE OUTPUT 4:
    Enter any number between 1 to 7:4
    THURSDAY
    
    SAMPLE OUTPUT 5:
    Enter any number between 1 to 7:5
    FRIDAY
    
    SAMPLE OUTPUT 6:
    Enter any number between 1 to 7:6
    SATURDAY
    
    SAMPLE OUTPUT 7:
    Enter any number between 1 to 7:7
    SUNDAY

---
+++ module_2/practical_6.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    printf("Enter any number between 1 to 7:");
+    scanf("%d",&n);
+    switch(n){
+        case 1:
+            printf("MONDAY");
+            break;
+        case 2:
+            printf("TUESDAY");
+            break;
+        case 3:
+            printf("WEDNESDAY");
+            break;
+        case 4:
+            printf("THURSDAY");
+            break;
+        case 5:
+            printf("FRIDAY");
+            break;
+        case 6:
+            printf("SATURDAY");
+            break;
+        case 7:
+            printf("SUNDAY");
+            break;
+        }
+    return 0;
+}
+
+
