795be186f4530c8fbe19368745c24ba90c305d47
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2022-01-29 16:22:39 +0530

    Create practical_10(C).c
    Q)WAP to print the following pattern.
    
    5 5 5 5 5 
    4 4 4 4
    3 3 3 
    2 2 
    1

---
+++ module_3/practical_10(C).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j,rows=5;
+    for(i=rows;i>=1;i--){
+        for(j=1;j<=i;j++){
+            printf("%d ",i);
+        }
+        printf("\n");
+    }
+}
